a3b6a3a31289c0f547343b910b84d2190156ccf4
WeiLong006 <lim.wei.long.1@gmail.com>
2023-02-01 17:10:33 +0800

    Updated About Us

--- src/comp-aboutUs/AboutUsPage.jsx
+++ src/comp-aboutUs/AboutUsPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const AboutUsPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>OUR STORY</h1>
+      <p>
+        We are Willing hearts and we run a soup kitchen that provides around
+        11,000 daily meals,island-wide 365 days a year (even on public
+        holidays). we have a "no questions asked" policy, just as you would for
+        your own family. That means that we provide meals to <bold>anyone</bold>{" "}
+        who needs it.
+      </p>
+      <br />
+      <p>
+        Our belief is that every deserves access to a warm meal and we are
+        committed to serving our community with compassion and respect. That's
+        the mission of Willing Hearts when it was founded in 2005. We were
+        awarded IPC(Institution of Public Character) status in2022.{" "}
+      </p>
+      <br />
+      <p></p>
+    </div>
+  );
 };
 
 export default AboutUsPage;
