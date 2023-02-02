fe8c1d612545c3ff8caf9591a5d4cd5e315dde00
WeiLong006 <lim.wei.long.1@gmail.com>
2023-02-02 17:14:01 +0800

    About Us Page completed

--- src/comp-aboutUs/Components/Journey.jsx
+++ src/comp-aboutUs/Components/Journey.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Journey = () => {
-  return <div></div>;
+const Journey = (props) => {
+  return (
+    <>
+      <img src={props.image}></img>
+      <label className="home">{props.homeName}</label>
+      <br />
+      <label className="year">{props.year}</label>
+      <br />
+      <label className="content">{props.content}</label>
+    </>
+  );
 };
 
 export default Journey;
