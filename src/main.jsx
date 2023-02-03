2bae40b60946d7a02470365233379b718e5e33f1
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-02-03 14:57:33 +0800

    Installed Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { Provider } from "react-redux";
-import store from "./store/main";
+// import { Provider } from "react-redux";
+// import store from "./store/main";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
