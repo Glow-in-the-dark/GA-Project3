85d3f15a5cd533d1224ad4917244022496383ac4
WeiLong006 <118900685+WeiLong006@users.noreply.github.com>
2023-02-05 00:09:24 +0800

    FAQ page completed

--- src/comp-aboutUs/Faq.jsx
+++ src/comp-aboutUs/Faq.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Faq = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div className="FAQ">
+        <h1 className="faqHeader">FREQUENTLY ASKED QUESTIONS</h1>
+        <label className="faqContent">
+          Here you will find answers to questions about our organistaion,
+          volunteering, donating to us and other topics related to our work
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default Faq;
