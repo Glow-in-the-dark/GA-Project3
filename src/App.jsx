9a4b2b45e6958a73878daeb752e09d599dea88a5
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-02-04 22:17:07 +0800

    ContactFormConfirmationPage completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import CommonComponentExamples from "./comp-commons/examplePage/CommonComponentExamples";
 import ContactUsPage from "./comp-contactUs/ContactUsPage";
+import ContactFormConfirmationPage from "./comp-contactUs/ContactFormConfirmationPage";
 
 function App() {
   return (
     <div>
       {/* <h1>GA SEI 41</h1> */}
       {/* <CommonComponentExamples /> */}
-      <ContactUsPage />
+      {/* <ContactUsPage /> */}
+      <ContactFormConfirmationPage />
     </div>
   );
 }
