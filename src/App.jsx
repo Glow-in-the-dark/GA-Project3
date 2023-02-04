bd6cba131d44b8bac4a829278c742eec1fececf7
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-02-04 11:20:54 +0800

    Edited Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "./comp-commons/Button";
+import BaseButton from "./comp-commons/BaseButton";
 
 function App() {
   return (
@@ -7,21 +7,21 @@ function App() {
       {/* Button */}
       <div className="m-5">
         <p className="text-4xl my-5">Button:</p>
-        <Button label="Volunteer Now" colour="red" />
+        <BaseButton label="Volunteer Now" colour="red" />
         <br />
-        <Button label="Find Out More" colour="white" />
+        <BaseButton label="Find Out More" colour="white" />
         <br />
-        <Button label="Sign Up" colour="white" />
+        <BaseButton label="Sign Up" colour="white" />
         <br />
-        <Button label="Join Us" colour="red" />
+        <BaseButton label="Join Us" colour="red" />
         <br />
-        <Button label="Donate" colour="red" />
+        <BaseButton label="Donate" colour="red" />
         <br />
-        <Button label="Next" colour="red" className="w-[294px]" />
+        <BaseButton label="Next" colour="red" className="w-[294px]" />
         <br />
-        <Button label="Add To Calendar" colour="white" icon="calendar" />
+        <BaseButton label="Add To Calendar" colour="white" icon="calendar" />
         <br />
-        <Button label="Invite Your Friends" colour="white" icon="share" />
+        <BaseButton label="Invite Your Friends" colour="white" icon="share" />
       </div>
     </div>
   );
