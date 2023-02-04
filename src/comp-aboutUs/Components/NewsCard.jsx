66014c35d0b3dd85fced36aa208daa9201959ed5
WeiLong006 <118900685+WeiLong006@users.noreply.github.com>
2023-02-05 00:26:28 +0800

    updated link to news

--- src/comp-aboutUs/Components/NewsCard.jsx
+++ src/comp-aboutUs/Components/NewsCard.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const NewsCard = (props) => {
   return (
     <>
-      <div className="newsCard">
-        <img className="newsImage" src={props.image}></img>
-        <label className="newsContent">{props.newsContent}</label>
-        <label className="newsSource">{props.newsSource}</label>
-        <label className="newsDate">{props.newsDate}</label>
-      </div>
+      <a href={props.newsLink}>
+        <div className="newsCard">
+          <img className="newsImage" src={props.image}></img>
+          <label className="newsContent">{props.newsContent}</label>
+          <label className="newsSource">{props.newsSource}</label>
+          <label className="newsDate">{props.newsDate}</label>
+        </div>
+      </a>
     </>
   );
 };
