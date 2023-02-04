bd32686ac46190bbaba2344f760204bc992589e5
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-02-04 10:22:42 +0800

    Edited Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Button from "./comp-commons/Button";
 
 function App() {
   return (
-    <div>
-      <h2>GA SEI-41</h2>
+    <div className="m-16">
+      {/* Button */}
+      <div className="m-5">
+        <p className="text-4xl my-5">Button:</p>
+        <Button label="Volunteer Now" colour="red" />
+        <br />
+        <Button label="Find Out More" colour="white" />
+        <br />
+        <Button label="Sign Up" colour="white" />
+        <br />
+        <Button label="Join Us" colour="red" />
+        <br />
+        <Button label="Donate" colour="red" />
+        <br />
+        <Button label="Next" colour="red" className="w-[294px]" />
+        <br />
+        <Button label="Add To Calendar" colour="white" icon="calendar" />
+        <br />
+        <Button label="Invite Your Friends" colour="white" icon="share" />
+      </div>
     </div>
   );
 }
