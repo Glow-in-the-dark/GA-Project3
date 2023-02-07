691903c4f967bdaf39a7f8b8e6fa7a0ba38be513
Gavin <Gavinlowa111989@gmail.com>
2023-02-07 14:27:36 +0800

    NavBar linkage done (need to work on CSS)

---
+++ src/comp-NavBar/MenuItems.jsx
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Projects",
+    path: "/projects",
+    cName: "dropdown-link",
+  },
+  {
+    title: "News",
+    path: "/news",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Faq",
+    path: "/faq",
+    cName: "dropdown-link",
+  },
+];
