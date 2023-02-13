c9d8f427656fc2397041198f6fd64e061ecead69
WeiLong006 <lim.wei.long.1@gmail.com>
2023-02-13 09:26:22 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import CommonComponentExamples from "./comp-commons/examplePage/CommonComponentE
 import ContactUsPage from "./comp-contactUs/ContactUsPage";
 import ContactFormConfirmationPage from "./comp-contactUs/ContactFormConfirmationPage";
 
-
 function App() {
   return (
     <div>
       <News />
-      {/* <h1>GA SEI 41</h1> */}
-      {/* <CommonComponentExamples /> */}
-      {/* <ContactUsPage /> */}
-      <ContactFormConfirmationPage />
+      {/* <CommonComponentExamples />
+      <ContactUsPage /> */}
+      {/* <ContactFormConfirmationPage /> */}
     </div>
   );
 }
