5e86e5302d8aa35376cb293c991cc156833070df
mrmuhdamir <whiteforestcat96@gmail.com>
2023-02-14 21:46:33 +0800

    How will it help done

--- src/comp-kitchen/TrainingKitchen.jsx
+++ src/comp-kitchen/TrainingKitchen.jsx
@@ -13,7 +13,7 @@ const TrainingKitchen = () => {
           <img
             src="./kitchenImages/stove.png"
             alt=""
-            className="mt-14 mx-auto mb-24 w-[1200px] h-[520px]"
+            className="mt-14 mx-auto mb-24 w-[1200px] h-[520px] rounded-2xl"
           />
         </p>
       </div>
@@ -24,7 +24,7 @@ const TrainingKitchen = () => {
             WHAT IS IT <br /> ABOUT?
           </h1>
           <div className="ml-28">
-            <p className="mb-9 text-justify w-[692px]">
+            <p className="mb-14 text-justify w-[692px]">
               Willing Hearts started this initiative to provide culinary
               education and job training to individuals in need. The training
               kitchen will provide hands-on training in the culinary arts and
@@ -41,7 +41,7 @@ const TrainingKitchen = () => {
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
+                <p className="text-center mt-3.5">
                   Have more confidence <br /> in their cooking abilities
                 </p>
               </li>
@@ -51,7 +51,7 @@ const TrainingKitchen = () => {
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
+                <p className="text-center mt-3.5">
                   See fewer barriers to <br /> making healthy, <br />
                   affordable meals
                 </p>
@@ -62,7 +62,7 @@ const TrainingKitchen = () => {
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
+                <p className="text-center mt-3.5">
                   Be able to prepare <br /> healthier, more budget- <br />
                   friendly meals
                 </p>
@@ -79,50 +79,49 @@ const TrainingKitchen = () => {
             <br /> IT HELP?
           </h1>
           <div className="ml-28">
-            <p className="mb-9 text-justify w-[692px]">
+            <p className="mb-14 text-justify w-[692px]">
               Evidence suggests that teaching cooking skills can help people
               better manage food insecurity by teaching them how to reduce food
               waste, budget and plan meals, and cook healthy meals with
               inexpensive ingredients. <br /> <br />
-              For low-income individuals, time and
-              finances can be barriers to continuous healthy cooking. The
-              program strives to help people navigate those challenges. The
-              program will also talk about allocating time to fixing up dishes
-              that will carry them throughout the week. The impact of using time
-              wisely to cook hearty, nutritious food will pay off in the long
-              run.
+              For low-income individuals, time and finances can be barriers to
+              continuous healthy cooking. The program strives to help people
+              navigate those challenges. The program will also talk about
+              allocating time to fixing up dishes that will carry them
+              throughout the week. The impact of using time wisely to cook
+              hearty, nutritious food will pay off in the long run.
             </p>
             <ul className="list-none flex justify-center space-x-28">
               <li>
                 <img
-                  src="./kitchenImages/smiley.png"
+                  src="./kitchenImages/2-persons.png"
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
-                  Have more confidence <br /> in their cooking abilities
+                <p className="text-center mt-3.5">
+                  This project aims to <br /> reach at least 1000 <br />
+                  people in the first year.
                 </p>
               </li>
               <li className="">
                 <img
-                  src="./kitchenImages/clock.png"
+                  src="./kitchenImages/handshake.png"
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
-                  See fewer barriers to <br /> making healthy, <br />
-                  affordable meals
+                <p className="text-center mt-3.5">
+                  We believe in paying it <br /> forward, each person <br /> we
+                  help goes on to <br /> help others in need.
                 </p>
               </li>
               <li>
                 <img
-                  src="./kitchenImages/dollar.png"
+                  src="./kitchenImages/goalpost.png"
                   alt=""
                   className="mx-auto"
                 />
-                <p className="text-center">
-                  Be able to prepare <br /> healthier, more budget- <br />
-                  friendly meals
+                <p className="text-center mt-3.5">
+                  We need to raise <br /> $10,000 to make <br /> this happen.
                 </p>
               </li>
             </ul>
